702f16f0bbf0aae87e1c8a55df81c71365feca84
C4U4NZIn <cauazindofree1234@gmail.com>
2024-03-19 16:36:23 -0300

    Cadastrojá feito , implementar landingPage

--- app/(public)/register/aluno/page.tsx
+++ app/(public)/register/aluno/page.tsx
@@ -10,227 +10,375 @@ import { userForm } from "../zod/validation";
 import { userFormSchema } from "../zod/validation";
 import Link from "next/link";
 import { Bounce, toast , ToastContainer} from "react-toastify";
+import styled from "styled-components";
+import CustomizedStepperIcon from "../../components/stepper";
+
+const ContainerSteps = styled.div<{$transformProps:number}>`
+display: flex;
+flex-direction: row;
+height: 300px;
+width: 96.75rem;
+
+border-radius: 10px;
+justify-content: stretch;
+transform: translateX(${props=>props.$transformProps}rem);
+transition: 500ms ease-in-out;
+`
+const DefaultContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: space-evenly;
+height: 300px;
+width: 32.25rem;
+
+border-radius: 10px;
+`
+const Button = styled.button`
+width: 11.25rem;
+height: 2.2rem;
+border-radius: 10px;
+background-color: rgba(242, 105, 33, 1);
+&:disabled{
+  background-color: transparent;
+}
+
+`
+const ContainerButtons = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-evenly;
+width: 100%;
+padding: 0;
+margin-top: 2rem;
+
+`
+const ContainerTitleStepInputs = styled.div`
+display: flex;
+flex-direction: column;
+gap:0.5rem;
+width: 100%;
+align-items: center;
+margin-top: 2rem;
+`
+const ContainerTitleStepper = styled.div`
+display: flex;
+flex-direction: column;
+gap:1.5rem;
+width: 100%;
+justify-content: center;
+align-items: center;
+`
+const ContainerTitleLine = styled.div`
+display: flex;
+flex-direction: column;
+gap:0.1rem;
+`
+const Line = styled.span`
+margin:0;
+padding: 0;
+height: 1px;
+width:12rem;
+background-color: blue;
+`
+const H1 = styled.h1`
+margin:0;
+padding: 0;
+color:black;
+
+`
+const H3 = styled.h3`
+margin:0;
+padding: 0;
+`
+const Span = styled.div`
+border-bottom: 1px solid blue;
+width: 8rem;
+`
+const ContainerInputTitle = styled.div`
+display: flex;
+flex-direction: column;
+gap:0.5rem;
+width: 60%;
+min-height: 5.625rem;
+`
+const Input = styled.input`
+height: 1.8125rem;
+width:100%;
+margin:0;
+padding-left: 10px;
+border-radius: 10px;
+background-color:  rgba(237, 237, 237, 1);
+border: none;
+`
+const TextError = styled.span`
+color:red;
+margin:0;
+padding:0;
+
+`
 
 export default function ProfileAluno(){
+   
+   const [role , setRole] = useState('');
     
-    const { createUser , user , jwtToken } = useUserContext();
-    const [role , setRole] = useState('');
-    useEffect(()=>{
-     const path = window.location.pathname;
-     const aluno = path.split('/register/')[1];
-     setRole(aluno);
-     }, [])
+   useEffect(()=>{
+   
+   const pathname = window.location.pathname;
+   const aluno = pathname.split('/register/')[1];
+   setRole(aluno);
 
-     const {
-        register,
-        handleSubmit,
-        watch,
-        control,
-        formState:{errors , touchedFields},
-        
-      } = useForm({
-        resolver: zodResolver(userFormSchema),
-        defaultValues: 
-        {
-          email:'',  
-          password:'',
-          confirmPassword:'',
-          username:'',
-          nickname:''
-        }
-       });
-       const notify = () => toast("Wow so easy!");
-       const handleSubmitValues = async (data:userForm)=>{
-      
-        try {
+   },[])
+
+   const [step, setStep] = useState(0);
+
+   const transformProps = -step*32.25 + 32.25;
+ 
+  const { createUser , jwtToken } = useUserContext();
+
+   const {
+     register,
+     handleSubmit,
+     watch,
+     getValues,
+     formState:{errors , touchedFields},
+     
+   } = useForm({
+     resolver: zodResolver(userFormSchema),
+     defaultValues: 
+     {
+       email:'',  
+       password:'',
+       confirmPassword:'',
+       username:'',
+       nickname:''
+     },
+     mode:'onChange'
+    });
+ 
+   const email = watch('email') && !errors.email;
+   const password = watch('password') && !errors.password;
+   const confirmPassword = watch('confirmPassword') && !errors.confirmPassword;
+   const username = watch('username') && !errors.username;
+   const nickname = watch('nickname') && !errors.nickname;
+   const isValidFieldStep2 = password && confirmPassword;
+   const isValidFieldStep3 = username && nickname;
+ 
+ 
+ 
+   const handleNextStep = (event:React.MouseEvent<HTMLButtonElement>) =>{
+     event.preventDefault();
+     setStep(step+1);
+   }
+ 
+   const handlePreviousStep = (event:React.MouseEvent<HTMLButtonElement>) =>{
+     event.preventDefault();
+     setStep(step-1);
+   }
+   const handleFormSubmit = async (data: userForm, event?: React.BaseSyntheticEvent) => {
+     event?.preventDefault();
+     try {
        
-            const values = {
-                username:data.username,
-                email:data.email,
-                nickname:data.nickname,
-                password:data.password,
-                role:role
-            }
+      const values = {
+          username:data.username,
+          email:data.email,
+          nickname:data.nickname,
+          password:data.password,
+          role:role
+      }
 
-         const response = await createUser(values);
-      
-         
-         toast.success( 'Você foi cadastrado com sucesso!',
-         {
-             position:'top-center',
-             autoClose:5000,
-             hideProgressBar: false,
-             closeOnClick: true,
-             pauseOnHover: true,
-             draggable: true,
-             progress: undefined,
-             theme: "colored",
-             transition: Bounce
-         });
-           
-          } catch (error) {
-      
-         console.log(error);
-            }
-        }
+   const response = await createUser(values);
+
+   
+   toast.success( 'Você foi cadastrado com sucesso!',
+   {
+       position:'top-center',
+       autoClose:5000,
+       hideProgressBar: false,
+       closeOnClick: true,
+       pauseOnHover: true,
+       draggable: true,
+       progress: undefined,
+       theme: "colored",
+       transition: Bounce
+   });
+     
+    } catch (error) {
+
+   console.log(error);
+      }
+     console.log(data);
+   };
+
           
 
     return(
-        <>
-       <div className={styles.agoraContainerForm}>
+        
+       <form
+   onSubmit={handleSubmit(handleFormSubmit)}
+   style={{
+   width:'32.25rem', 
+   height:'579px', 
+   display:"flex", 
+   flexDirection:'column',
+   alignItems:'center',
+   border:'5px solid gray',
+   borderRadius:'10px',
+   overflow:'hidden',
+   marginLeft:'100px',
+   marginTop:'90px',
+   backgroundColor:'#fff',
+   gap:'2rem'
 
-<div className={styles.agoraContentCadastroLineForm}>
-
-<div className={styles.agoraContainerLines}>
-
-<span className={styles.agoraCadastroLine}>Cadastro</span>
-<a href="" className={styles.agoraCadastroFirstLine}></a>
-<a href="" className={styles.agoraCadastroSecondLine}></a>
-
-</div>
+   }}>
 
 
-<form onSubmit={handleSubmit(handleSubmitValues)}>
+ <ContainerTitleStepInputs>
+   <ContainerTitleStepper>
+    <ContainerTitleLine>
+    <H1>Cadastro</H1>
+    <Span></Span>
+    <Line></Line>
+    </ContainerTitleLine>
+  <CustomizedStepperIcon
+  currentStep={step}
+  />
+   </ContainerTitleStepper>
 
-<div className={styles.agoraContainsAllFormElements}>
+     <ContainerSteps
+    $transformProps={transformProps}
+    >
+    <DefaultContainer>
+     
+
+     <ContainerInputTitle>
+
+      <H3>Email</H3>
+     <Input
+      {...register('email')}
+      type= 'email'
+      placeholder='' 
+     />
+     {
+       errors.email && (
+        <TextError>{errors.email.message}</TextError>
+      )
+     }
+     </ContainerInputTitle>
+
     
-   <div className={styles.agoraContainerInputsButton}>
+    <Button onClick={handleNextStep} disabled={!email}>
+      proximo
+    </Button>
+      
+   
+    </DefaultContainer>
+    <DefaultContainer>
 
-    {/** Email input */}
-  <InputLabelContainer>
-  <Label>Email</Label>
-  <InputErrorMessage>
-   <InputForm
-     {...register('email')}
-     type= 'email'
-     placeholder='' 
-     id="agoraEmail"
+ 
 
-   />
-   { errors.email && (
-      <ErrorMessage>{errors.email.message}</ErrorMessage>
-   )}
+    <ContainerInputTitle>
 
-  </InputErrorMessage>
+      <H3>Senha</H3>
+    <Input
+       {...register('password')}
+       type= 'password'
+       placeholder='' 
+     
+     />
+      {
+       errors.password && (
+        <TextError>{errors.password.message}</TextError>
+      )
+     }
+    </ContainerInputTitle>
+   
 
+     <ContainerInputTitle>
+
+      <H3>Confirmar Senha</H3>
+       <Input
+       {...register('confirmPassword')}
+       type= 'password'
+       placeholder='' 
+     
+     />
+       {
+       errors.confirmPassword && (
+        <TextError>{errors.confirmPassword.message}</TextError>
+      )
+     }
+     </ContainerInputTitle>
   
-  </InputLabelContainer>
 
 
-   {/** Senha input */}
-  <InputLabelContainer>
-  <Label>Senha</Label>
-  <InputErrorMessage>
-   <InputForm
-     {...register('password')}
-     type= 'password'
-     placeholder='' 
-     id="agoraUserPassword"
+    <ContainerButtons>
+   
+    <Button onClick={handlePreviousStep}>
+        anterior
+      </Button>
+  
+   <Button onClick={handleNextStep} disabled={!isValidFieldStep2}>
+      proximo
+    </Button>
 
-   />
-   { errors.password && (
-      <ErrorMessage>{errors.password.message}</ErrorMessage>
-   )}
-  </InputErrorMessage>
+    </ContainerButtons>
+      
+    </DefaultContainer>
+    <DefaultContainer>
+      
 
-  </InputLabelContainer>
+      <ContainerInputTitle>
 
-   {/** Confirm Password input */}
-  <InputLabelContainer>
-  <Label>Confirmar Senha</Label>
-  <InputErrorMessage>
-   <InputForm
-     {...register('confirmPassword')}
-     type= 'password'
-     placeholder='' 
-     id="agoraUserConfirmPassword"
+      <H3>Nome</H3>
+    <Input  
+    {...register('username')}
+     type= 'text'
+     placeholder='' />
 
-   />
-   { errors.confirmPassword && (
-      <ErrorMessage>{errors.confirmPassword.message}</ErrorMessage>
-   )}
-  </InputErrorMessage>
-  </InputLabelContainer>
+    {
+       errors.username && (
+        <TextError>{errors.username.message}</TextError>
+      )
+     }
+      </ContainerInputTitle>
+   
+   <ContainerInputTitle>
 
-  {/** Nome input */}
-  <InputLabelContainer>
-  <Label>Nome</Label>
-  <InputErrorMessage>
-   <InputForm
-     {...register('username')}
+    <H3>Apelido</H3>
+    <Input 
+    {...register('nickname')}
      type= 'text'
      placeholder='' 
-     id="agoraUserUsername"
+     />
 
-   />
-   { errors.username && (
-      <ErrorMessage>{errors.username.message}</ErrorMessage>
-   )}
-  </InputErrorMessage>
-  </InputLabelContainer>
-
-     {/** Nickname input */}
-     <InputLabelContainer>
-  <Label>Apelido</Label>
-  <InputErrorMessage>
-   <InputForm
-     {...register('nickname')}
-     type= 'text'
-     placeholder='' 
-     id="agoraUserNickname"
-   />
-   { errors.nickname && (
-      <ErrorMessage>{errors.nickname.message}</ErrorMessage>
-   )}
-  </InputErrorMessage>
-  </InputLabelContainer>
+{
+       errors.nickname && (
+        <TextError>{errors.nickname.message}</TextError>
+      )
+     }
+   </ContainerInputTitle>
 
 
+        <ContainerButtons>
+      <Button onClick={handlePreviousStep}>
+        anterior
+      </Button>
+     
+      <Button type="submit" disabled={!isValidFieldStep3}>
+         Criar Conta
+        </Button>
 
-<div 
-className={styles.agoraContainerButtonForm}
->
+        </ContainerButtons>
+  
+      </DefaultContainer>
+    </ContainerSteps>
 
-<button type="submit"  
-className={styles.agoraButtonCancel}>
-<p className={styles.agoraColorLetterForm}
->
-Cancelar
-</p>
-</button>
+ </ContainerTitleStepInputs>
 
-<button type="submit" 
-
-className={styles.agoraButtonCad}>
-<p 
-className={styles.agoraColorLetterForm}
->
-Cadastrar
-</p>
-</button>
-</div>
-   </div>
-</div>
-
-<div 
-className={styles.agoraContainerLinkLogin}
->
-<Link 
-className={styles.agoraStylesLink} 
-href="/Login"
->
-Já tenho Login
-</Link>
-</div>
-
-
-</form>
-
-
-</div>
-
-
-</div>
-        </>
+    <Link href='/SignUp'>Já tenho Login</Link>
+    
+    
+   </form>
     )
 }
